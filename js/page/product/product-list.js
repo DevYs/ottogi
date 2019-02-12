4dcc884929b910347e8fec74a65e22043071cddb
devy <hanys1042@gmail.com>
2019-02-12 22:25:25 +0900

    product json

--- js/page/product/product-list.js
+++ js/page/product/product-list.js
@@ -3,6 +3,7 @@
     변수
 ******/
 var URL_CATEGORY_DEPTH2 = "https://devys.github.io/ottogi/js/category.json";
+var DEPTH_2_LIST = "";
 
 $(document).ready(function() {
 
@@ -13,7 +14,7 @@ $.ajax({
     url:URL_CATEGORY_DEPTH2,
     type:'get',
     success:function(data) {
-        console.log(data);
+        DEPTH_2_LIST = data;
     },
     error:function(e) {
         console.log(e);
@@ -24,6 +25,23 @@ $.ajax({
 /********
     이벤트
 ********/
+$("#depth1").on("change", function() {
+    var depth1 = $(this).find("option:selected").val();
+    var tag = "<option value='{code}'>{name}</option>";
+
+    $('#depth2').empty();
+    $('#depth2').append(tag.replace('{code}', '').replace('{name}', '선택하세요'));
+
+    for(var i=0; i<DEPTH_2_LIST.length; i++) {
+        var depth2 = DEPTH_2_LIST[i];
+        if(depth2.parentCode == depth1) {
+            var temp = tag.replace('{code}', depth2.code)
+                          .replace('{name}', depth2.name);
+            $('#depth2').append(temp);
+        }
+    }
+});
+
 
 });
 
